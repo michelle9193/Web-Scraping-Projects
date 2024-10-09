46126657a8abc648997ea111d532adfbf23fc388
michellefortuin93 <michellefortuin93@gmail.com>
2024-10-09 13:55:56 +0200

    add weather app project

---
+++ weather.py
@@ -0,0 +1,13 @@
+from requests_html import HTMLSession
+
+s = HTMLSession()
+query = 'london'
+url = f'https://www.google.com/search?q=weather+{query}'
+
+r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'})
+
+temp = r.html.find('span#wob_tm',first=True).text
+unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text
+condition = r.html.find('div#wob_dcp span#wob_dc', first=True).text
+
+print(f"{query.title()}: {temp}{unit} {condition}")
