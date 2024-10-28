59f596b0799bbf23c81affcdc65b9afffac6052d
michellefortuin93 <michellefortuin93@gmail.com>
2024-10-28 21:48:01 +0200

    update

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 from requests_html import HTMLSession
 
 s = HTMLSession()
-query = 'london'
+query = input("Enter the name of a city to check the weather. ")
 url = f'https://www.google.com/search?q=weather+{query}'
 
 r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'})
